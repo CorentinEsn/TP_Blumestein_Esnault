8563c5a219f9fb88fc42799a2f5b00aad1261190
CorentinEsn <75800644+CorentinEsn@users.noreply.github.com>
2021-12-15 16:46:42 +0100

    Updated UI (replaced TableView by TextFlow)

--- src/main/java/com/example/tp_blumestein_esnault/MainApplication.java
+++ src/main/java/com/example/tp_blumestein_esnault/MainApplication.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.sql.*;
+import com.example.tp_blumestein_esnault.MainController;
 
 import java.io.IOException;
 
@@ -17,6 +18,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 615, 400);
+        MainController mainController = fxmlLoader.getController();
         stage.setTitle("Reservation de salles");
         stage.setScene(scene);
         stage.show();
